eb074b04adaefc6433411ba07a62e81672046b0d
nokchax <nokchax@gmail.com>
2020-05-26 00:01:05 +0900

    feat : use functional interface to match prize

--- src/main/java/lotto/prize/LottoPrize.java
+++ src/main/java/lotto/prize/LottoPrize.java
@@ -1,42 +1,33 @@
 package lotto.prize;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+import java.util.function.BiFunction;
 import java.util.stream.Stream;
 
 public enum LottoPrize {
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
-    FIFTH(3, 5_000),
-    NONE(0, 0);
+    FIRST(6, 2_000_000_000, (matchCount, bonusMatch) -> matchCount == 6),
+    SECOND(5, 30_000_000, (matchCount, bonusMatch) -> matchCount == 5 && bonusMatch),
+    THIRD(5, 1_500_000, (matchCount, bonusMatch) -> matchCount == 5 && !bonusMatch),
+    FOURTH(4, 50_000, (matchCount, bonusMatch) -> matchCount == 4),
+    FIFTH(3, 5_000, (matchCount, bonusMatch) -> matchCount == 3),
+    NONE(0, 0, (matchCount, bonusMatch) -> matchCount < 3);
 
     private final int matchCount;
     private final int prizeMoney;
+    private final BiFunction<Integer, Boolean, Boolean> matcher;
 
-    private static final Map<Integer, LottoPrize> PRIZES = Collections.unmodifiableMap(
-            Stream.of(values())
-                    .collect(Collectors.toMap(LottoPrize::getMatchCount, Function.identity(), ((pre, post) -> post)))
-    );
-
-    LottoPrize(final int matchCount, final int prizeMoney) {
+    LottoPrize(final int matchCount, final int prizeMoney, final BiFunction<Integer, Boolean, Boolean> matcher) {
         this.matchCount = matchCount;
         this.prizeMoney = prizeMoney;
+        this.matcher = matcher;
     }
 
     public static LottoPrize of(final int matchCount, final boolean isBonusMatch) {
-        LottoPrize prize = PRIZES.getOrDefault(matchCount, NONE);
-
-        if (prize == THIRD && isBonusMatch) {
-            return SECOND;
-        }
-
-        return prize;
+        return Stream.of(values())
+                .filter(prize -> prize.matcher.apply(matchCount, isBonusMatch))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Match info is not illegal"));
     }
 
     public static List<LottoPrize> getMeaningfulPrize() {
